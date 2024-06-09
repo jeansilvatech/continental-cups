8706daf5fb7adfa767e552a94b09defbbe10f0db
jeansilvatech <jeanpsilva10@gmail.com>
2024-06-08 22:54:34 -0300

    [style] ajuste de alinhamento nos cards de grupos

--- src/components/SelectionsOfGroup/index.tsx
+++ src/components/SelectionsOfGroup/index.tsx
@@ -9,7 +9,7 @@ interface SelectionsOfGroupProps{
 }
 const SelectionsOfGroup = ({name, flag, color, originImage}:SelectionsOfGroupProps)=>{
     return(
-        <div className="m-3 flex justify-center items-center flex-col">
+        <div className="m-3 flex justify-center items-center flex-col w-1/3">
             <Image className="bg-[#D9D9D9] rounded-full lg:w-[70px] lg:h-[70px] w-10 h-10" src={`/assets/${originImage}/${flag}.svg`} width={70} height={70} alt={`bandeira: ${name}`}/>
             <span className={`uppercase text-xs text-center mt-1 font-semibold whitespace-nowrap text-${color}`}>{name}</span>
         </div>
