b6aa939f5fe592191133296d476da0e0f8569f4b
jeansilvatech <jeanpsilva10@gmail.com>
2024-06-13 12:35:01 -0300

    [style] add after nos cards da home para informar que ao clicar vai para a página da Copa

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -12,12 +12,16 @@ const HomePage = ()=>{
             <div onClick={()=>{router.push('copa-america')}} className="lg:w-1/2 lg:h-full w-full h-1/2 bg-america flex justify-center items-center cursor-pointer relative
             before:content-[''] before:absolute before:w-full before:h-1/2 before:top-0 before:left-0 before:bg-gradient-to-b from-america to-transparent
             hover:bg-white
+            after:content-[''] lg:after:text-base after:text-xs after:absolute after:bottom-0 after:w-full after:p-3 after:bg-america1 after:text-white after:uppercase after:hidden
+            hover:after:flex after:justify-center after:items-center hover:after:animate-enter hover:after:content-['Visualizar_a_tabela']
             ">
                 <Image className="object-contain object-center z-50" src={logoCopaAmerica} width={200} height={200} alt=""/>
             </div>
             <div onClick={()=>{router.push('eurocopa')}} className="lg:w-1/2 lg:h-full w-full h-1/2 bg-euro flex justify-center items-center cursor-pointer relative
             before:content-[''] before:absolute before:w-full before:h-1/2 before:bottom-0 before:left-0 before:bg-gradient-to-t from-euro
-            hover:bg-white">
+            hover:bg-white
+            after:content-[''] lg:after:text-base after:text-xs after:absolute after:top-0 after:w-full after:p-3 after:bg-euro1 after:text-white after:uppercase after:hidden
+            hover:after:flex after:justify-center after:items-center hover:after:animate-enter hover:after:content-['Visualizar_a_tabela']">
                 <Image className="object-contain object-center z-50" src={logoEuroCopa} width={160} height={200} alt=""/>
             </div>
         </div>
