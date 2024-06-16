84e1e09e5323587ba9f6e880b3288ff235c9ec6f
jeansilvatech <jeanpsilva10@gmail.com>
2024-06-16 15:13:34 -0300

    [style] ajuste de estilo na página da Copa

--- src/app/eurocopa/page.tsx
+++ src/app/eurocopa/page.tsx
@@ -78,7 +78,7 @@ const PageEuroCopa = ()=>{
                             <TitleCardPhase title={item.phase} color="euro"/>
                             {
                                 item.matches.map((match:any, index:number)=>(
-                                    <PhaseConfrontation roundNumber={match.round} numberConfrontation={match.decision?match.decision:index+1} key={index} selection1={match.name1} gols1={match.gols1} flag1={match.flagUrl1} selection2={match.name2} gols2={match.gols2} flag2={match.flagUrl2} color="text-euro" colorBorderMatchNumber="euro" originImage="euro"/>
+                                    <PhaseConfrontation roundNumber={match.round} numberConfrontation={match.decision?match.decision:index+1} key={index} selection1={match.name1} gols1={match.gols1} flag1={match.flagUrl1} selection2={match.name2} gols2={match.gols2} flag2={match.flagUrl2} color="text-euro" colorBorderMatchNumber="border-euro" originImage="euro"/>
                                 ))
                             }
 
--- src/components/PhaseConfrontation/index.tsx
+++ src/components/PhaseConfrontation/index.tsx
@@ -10,7 +10,7 @@ interface PhaseConfrotationProps {
     gols2: string;
     flag2: string;
     color: string;
-    colorBorderMatchNumber: "euro"| "america";
+    colorBorderMatchNumber: "border-euro"| "border-america";
     originImage: "euro"| "america";
     numberConfrontation: number | string;
     roundNumber: number;
@@ -30,7 +30,7 @@ const PhaseConfrontation = ({selection1, gols1, flag1, selection2, gols2, flag2,
         }
 
         <div className={`lg:w-1/3 w-4/5 flex justify-center items-center bg-[#E9E9E9] py-3 px-2 lg:m-5 m-2 shadow-md rounded-lg uppercase relative`}>
-            <span className={`absolute -top-2 bg-[#E9E9E9] flex justify-center items-center text-xs font-semibold w-5 h-5 border-${colorBorderMatchNumber} border-[1px] rounded-full ${color}`}>{numberConfrontation}</span>
+            <span className={`absolute -top-2 bg-[#E9E9E9] flex justify-center items-center text-xs font-semibold w-6 h-6 ${colorBorderMatchNumber} border-2 rounded-full ${color}`}>{numberConfrontation}</span>
             <div  className="flex flex-1 justify-center items-center flex-col">
                 <Image className="bg-[#D9D9D9] rounded-full lg:w-[50px] lg:h-[50px] w-10 h-10" src={`/assets/${originImage}/${flag1}.svg`} width={50} height={50} alt=""/>
                 <span className={`text-xs ${color} font-semibold mt-1`}>{selection1}</span>
