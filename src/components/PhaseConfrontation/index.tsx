58c48acdcaad229fc10b4ba425b0ae39ce48514a
jeansilvatech <jeanpsilva10@gmail.com>
2024-06-16 12:25:02 -0300

    [style] ajuste de estilo no card de confronto

--- src/components/PhaseConfrontation/index.tsx
+++ src/components/PhaseConfrontation/index.tsx
@@ -10,11 +10,12 @@ interface PhaseConfrotationProps {
     gols2: string;
     flag2: string;
     color: string;
+    colorBorderMatchNumber: "euro"| "america";
     originImage: "euro"| "america";
     numberConfrontation: number | string;
     roundNumber: number;
 }
-const PhaseConfrontation = ({selection1, gols1, flag1, selection2, gols2, flag2, color, originImage, numberConfrontation, roundNumber}:PhaseConfrotationProps)=>{
+const PhaseConfrontation = ({selection1, gols1, flag1, selection2, gols2, flag2, color, colorBorderMatchNumber, originImage, numberConfrontation, roundNumber}:PhaseConfrotationProps)=>{
     return(
         <>
         {
@@ -29,13 +30,13 @@ const PhaseConfrontation = ({selection1, gols1, flag1, selection2, gols2, flag2,
         }
 
         <div className={`lg:w-1/3 w-4/5 flex justify-center items-center bg-[#E9E9E9] py-3 px-2 lg:m-5 m-2 shadow-md rounded-lg uppercase relative`}>
-            <span className={`absolute -top-3 bg-[#E9E9E9] text-xs font-bold px-3 py-1 rounded-t-full ${color}`}>{numberConfrontation}</span>
+            <span className={`absolute -top-2 bg-[#E9E9E9] flex justify-center items-center text-xs font-semibold w-5 h-5 border-${colorBorderMatchNumber} border-[1px] rounded-full ${color}`}>{numberConfrontation}</span>
             <div  className="flex flex-1 justify-center items-center flex-col">
                 <Image className="bg-[#D9D9D9] rounded-full lg:w-[50px] lg:h-[50px] w-10 h-10" src={`/assets/${originImage}/${flag1}.svg`} width={50} height={50} alt=""/>
                 <span className={`text-xs ${color} font-semibold mt-1`}>{selection1}</span>
             </div>
             <div className="flex justify-center items-center">
-            <span className={`font-bold text-xl ${color}`}>{gols1}</span>
+            <span className={`font-bold text-xl h-full ${color}`}>{gols1}</span>
             <Close sx={{fontSize:40}} className={`${color} mx-5`}/>
             <span className={`font-bold text-xl ${color}`}>{gols2}</span>
             </div>
